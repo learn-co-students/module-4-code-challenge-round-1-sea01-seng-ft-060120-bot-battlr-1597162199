e188eb4efae7926ead452dd6a16a1d8f5b8ac0b6
carcarvroom <carly_l@outlook.com>
2020-08-11 10:43:18 -0700

    core deliverables complete

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,71 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  state = {
+    allBots: [],
+    botCollection: [],
+    botArmy: []
+  }
+
+  async componentDidMount() {
+    try {
+      let res = await fetch('http://localhost:6001/bots')
+      let data = await res.json()
+      this.setState({
+        allBots: data,
+        botCollection: data
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  addToArmy = (bot) => {
+    if(!this.state.botArmy.some(bots => bots.id === bot.id)) {
+      this.setState({
+        botArmy: [...this.state.botArmy, bot]
+      })
+    } else {
+      alert('That bot has already enlisted in your army.')
+    }
+  }
+
+  removeFromArmy = (bot) => {
+    let botArmy = this.state.botArmy.filter(bots => bots.id!==bot.id)
+    this.setState({
+      botArmy
+    })
+  }
+
+  deleteBot = async (bot) => {
+    try {
+      await fetch(`http://localhost:6001/bots/${bot.id}`, {method: 'DELETE'})
+      let newBots = this.state.allBots.filter(bots => bots.id !== bot.id )
+      this.setState({
+        botCollection: newBots
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+      <div>
+          {<YourBotArmy 
+            botArmy={this.state.botArmy} 
+            removeBot={this.removeFromArmy} 
+            deleteBot={this.deleteBot}/>
+          }
+          {<BotCollection 
+            botCollection={this.state.botCollection} 
+            addBot={this.addToArmy} 
+            deleteBot={this.deleteBot}/>
+          }
+      </div>
+    )
   }
 }
 
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard'
 
 class YourBotArmy extends Component {
   //your bot army code here...
@@ -6,10 +7,10 @@ class YourBotArmy extends Component {
   render() {
     return (
       <div className="ui segment inverted olive bot-army">
+        <h2>Bot Army</h2>
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            {this.props.botArmy.map(bot => <BotCard key={bot.id} bot={bot} addOrRemoveBot={this.props.removeBot} deleteBot={this.props.deleteBot}/>)}
           </div>
         </div>
       </div>
